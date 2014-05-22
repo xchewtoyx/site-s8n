05510cc5ecefa4633881e5d13e19bc3e1b82e257
Russell Heilling <russell@linx.net>
2014-05-23 00:48:18 +0100

    Make the breadcrumb link code more robust

--- appengine/sphinx.py
+++ appengine/sphinx.py
@@ -33,11 +33,22 @@ class SitemapHandler(webapp.RequestHandler):
 
 class SphinxJsonHandler(webapp.RequestHandler):
   def _fixup_parents(self, context):
+    # the parents list is a mess.  The list gives relative urls, with
+    # no file extension and has a habit of stripping 'index' for index
+    # pages even though it is inluded in the 'current_page_name' of
+    # the relevant page.  Ugh.
     parents = context['parents']
     current_path = os.path.dirname(context['current_page_name'])
     for index in range(len(parents)):
-      parents[index]['link'] = os.path.relpath(
-        os.path.join('/', current_path, parents[index]['link']))
+      parent = parents[index]['link']
+      # Work out the relative path difference
+      link_path = os.path.join('/', current_path, parent)
+      link_path = os.path.relpath(link_path)
+      # if the relative link ends with a /, then ensure the link_path
+      # does too
+      if parent.endswith('/') and not link_path.endswith('/'):
+        link_path = link_path + '/'
+      parents[index]['link'] = link_path
 
   def get(self, document_path):
     # Fixup the request path
